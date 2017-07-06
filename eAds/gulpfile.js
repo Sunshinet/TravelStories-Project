ee25eed2302f86edbbb2a9547318e4fa9ea6be65
Lucho Gizdov <olebg01@gmail.com>
2017-07-06 17:32:38 +0300

    rework project base structure and some fixes for eslint

--- eAds/gulpfile.js
+++ eAds/gulpfile.js
@@ -1,26 +1,28 @@
-var gulp = require('gulp'),
-  nodemon = require('gulp-nodemon'),
-  plumber = require('gulp-plumber'),
-  livereload = require('gulp-livereload');
+/* globals __dirname stdout stderr process*/ 
+
+const gulp = require('gulp');
+const nodemon = require('gulp-nodemon');
+// const plumber = require('gulp-plumber');
+const livereload = require('gulp-livereload');
 
 
-gulp.task('develop', function () {
+gulp.task('develop', function() {
   livereload.listen();
   nodemon({
     script: 'bin/www',
     ext: 'js jade coffee',
-    stdout: false
-  }).on('readable', function () {
-    this.stdout.on('data', function (chunk) {
-      if(/^Express server listening on port/.test(chunk)){
+    stdout: false,
+  }).on('readable', function() {
+    stdout.on('data', function(chunk) {
+      if (/^Express server listening on port/.test(chunk)) {
         livereload.changed(__dirname);
       }
     });
-    this.stdout.pipe(process.stdout);
-    this.stderr.pipe(process.stderr);
+    stdout.pipe(process.stdout);
+    stderr.pipe(process.stderr);
   });
 });
 
 gulp.task('default', [
-  'develop'
+  'develop',
 ]);
