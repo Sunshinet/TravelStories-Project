a647f1e754b6c6a9f6d79b76863c2dda11bfb195
Lucho Gizdov <olebg01@gmail.com>
2017-07-24 21:43:10 +0300

    add nodemon, but for now not tracking changes

--- TravelStoriesApp/gulpfile.js
+++ TravelStoriesApp/gulpfile.js
@@ -1,9 +1,26 @@
+/* globals stdout __dirname stderr process */
+
 const gulp = require('gulp');
+const nodemon = require('gulp-nodemon');
+const livereload = require('gulp-livereload');
 
 gulp.task('server:start', () => {
   return require('./server');
 });
 
-gulp.task('default', [
-  'develop',
-]);
+gulp.task('nodemon:start', function() {
+  livereload.listen();
+  nodemon({
+    script: './server.js',
+    ext: 'js pug',
+    stdout: false,
+  }).on('readable', function() {
+    stdout.on('data', function(chunk) {
+      if (/^Express server listening on port/.test(chunk)) {
+        livereload.changed(__dirname);
+      }
+    });
+    stdout.pipe(process.stdout);
+    stderr.pipe(process.stderr);
+  });
+});
