6383241e0655db3e370f0b3c2b9ed6edc91e19fb
Lucho Gizdov <olebg01@gmail.com>
2017-07-24 21:19:47 +0300

    cleanup of gulp and package json files

--- TravelStoriesApp/gulpfile.js
+++ TravelStoriesApp/gulpfile.js
@@ -1,28 +1,7 @@
-/* globals __dirname stdout stderr process*/ 
-
 const gulp = require('gulp');
-const nodemon = require('gulp-nodemon');
-// const plumber = require('gulp-plumber');
-const livereload = require('gulp-livereload');
 
 gulp.task('server:start', () => {
   return require('./server');
-});
-gulp.task('develop', function() {
-  livereload.listen();
-  nodemon({
-    script: 'bin/www',
-    ext: 'js jade coffee',
-    stdout: false,
-  }).on('readable', function() {
-    stdout.on('data', function(chunk) {
-      if (/^Express server listening on port/.test(chunk)) {
-        livereload.changed(__dirname);
-      }
-    });
-    stdout.pipe(process.stdout);
-    stderr.pipe(process.stderr);
-  });
 });
 
 gulp.task('default', [
