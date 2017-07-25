0cfcf6ddc3d4731ffbc3ccb447daf01526686e83
Lucho Gizdov <olebg01@gmail.com>
2017-07-25 19:51:35 +0300

    revert back mongo ip

--- TravelStoriesApp/server.config/server.config.js
+++ TravelStoriesApp/server.config/server.config.js
@@ -1,6 +1,6 @@
 const port = 3000;
 const connectionString = 'mongodb://localhost/stories';
-const connectionStringAWS = 'mongodb://13.59.102.61/stories';
+const connectionStringAWS = 'mongodb://172.31.19.21/stories';
 const sessionSecret = 'secretSession';
 
 module.exports = { port, connectionString, connectionStringAWS, sessionSecret };
