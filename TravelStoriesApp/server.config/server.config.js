ac255bcc90be32ec78d6aec31c2fd23beabf7f99
Lucho Gizdov <olebg01@gmail.com>
2017-07-26 00:28:03 +0300

    change ip in connection string

--- TravelStoriesApp/server.config/server.config.js
+++ TravelStoriesApp/server.config/server.config.js
@@ -1,6 +1,6 @@
 const port = 3000;
 const connectionString = 'mongodb://localhost/stories';
-const connectionStringAWS = 'mongodb://172.31.19.21/stories';
+const connectionStringAWS = 'mongodb://13.59.147.172/stories';
 const sessionSecret = 'secretSession';
 
 module.exports = { port, connectionString, connectionStringAWS, sessionSecret };
