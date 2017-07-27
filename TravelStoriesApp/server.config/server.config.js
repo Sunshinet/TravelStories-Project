53e61a9ca78801a6b2af5ab765096f0b11bf9c09
Lucho Gizdov <olebg01@gmail.com>
2017-07-27 15:44:31 +0300

    change port

--- TravelStoriesApp/server.config/server.config.js
+++ TravelStoriesApp/server.config/server.config.js
@@ -1,4 +1,4 @@
-const port = 3000;
+const port = 80;
 const connectionString = 'mongodb://localhost/stories';
 const connectionStringAWS = 'mongodb://13.59.102.61/stories';
 const sessionSecret = 'secretSession';
